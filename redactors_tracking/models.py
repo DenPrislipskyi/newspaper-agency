a0b34709c8c0d8d6f5d973cd2ca49b8ce959e091
Denys <den.prislipskyi@gmail.com>
2023-11-21 21:23:26 +0100

    add Newspaper and Redactor models

--- redactors_tracking/models.py
+++ redactors_tracking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 class Topic(models.Model):
@@ -9,3 +10,29 @@ class Topic(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=125)
+    content = models.TextField(null=True, blank=True)
+    published_data = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name="newspapers")
+    publishers = models.ManyToManyField("Redactor", related_name="publishers")
+
+    class Meta:
+        ordering = ["-published_data"]
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+
+class Redactor(AbstractUser):
+    redactor_id = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name}, {self.last_name})"

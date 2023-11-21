dc41058c575f49b4850ff82a7ca1a00b864b1ba4
Denys <den.prislipskyi@gmail.com>
2023-11-21 21:21:50 +0100

    add Topic model

--- redactors_tracking/models.py
+++ redactors_tracking/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=55, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name

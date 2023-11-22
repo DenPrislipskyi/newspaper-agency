8d0b5cf1da21a58e625797dcd731cd516c8879be
Denys <den.prislipskyi@gmail.com>
2023-11-22 10:35:08 +0100

    Add admin panel configurations for Redactor Newspaper and Topic models

--- redactors_tracking/admin.py
+++ redactors_tracking/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from redactors_tracking.models import Redactor, Newspaper, Topic
 
-# Register your models here.
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("redactor_id",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("redactor_id",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "redactor_id",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    search_fields = ("title",)
+    list_filter = ("topic",)
+
+
+admin.site.register(Topic)

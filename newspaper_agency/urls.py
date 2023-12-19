f017095444b2f364668b2f698e0da49822a87126
Denys <den.prislipskyi@gmail.com>
2023-12-19 22:59:45 +0100

    refactor code and flake8

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -19,7 +19,11 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("redactors_tracking.urls", namespace="redactors_tracking")),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("admin/",
+         admin.site.urls),
+    path("",
+         include("redactors_tracking.urls",
+                 namespace="redactors_tracking")),
+    path("accounts/",
+         include("django.contrib.auth.urls")),
 ]

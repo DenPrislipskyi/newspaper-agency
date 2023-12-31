f17cfa024fbe164c977500c2fa14a9686cf78072
Denys <den.prislipskyi@gmail.com>
2023-12-31 15:31:36 +0100

    fix static and fix search form for newspapers

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -133,12 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = "staticfiles/"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_URL = '/static/'
+# STATIC_ROOT = "staticfiles/"
+STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 ASSETS_ROOT = "static/assets"
 
